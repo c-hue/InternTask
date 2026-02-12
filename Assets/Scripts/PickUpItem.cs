c532d455caae3925c301fab17b38db96f16f4d3e
jadethegemstone <143663485+jadethegemstone@users.noreply.github.com>
2026-02-11 17:47:49 -0800

    Updating PickUp thing

--- Assets/Scripts/PickUpItem.cs
+++ Assets/Scripts/PickUpItem.cs
@@ -9,9 +9,9 @@ public class PickUpItem : MonoBehaviour
     private bool holdingItem;
     private Item itemType;
     [SerializeField] GameObject item;
+    [SerializeField] Sprite coffee;
     [SerializeField] Sprite water;
     [SerializeField] Sprite paper;
-    [SerializeField] Sprite coffee;
     [SerializeField] Sprite clipboard;
     [SerializeField] Sprite stapler;
     [SerializeField] Sprite pencilHolder;
@@ -27,42 +27,44 @@ public class PickUpItem : MonoBehaviour
     {
         if (inPickUpZone)
         {
-            if (Keyboard.current.spaceKey.wasPressedThisFrame)
-            {   
-                holdingItem = true;
-                switch (itemType.type)
-                {
-                    case ItemType.Water:
-                        item.GetComponent<SpriteRenderer>().sprite = water;
-                        break;
-                    case ItemType.Paper:
-                        item.GetComponent<SpriteRenderer>().sprite = paper;
-                        break;
-                    case ItemType.Coffee:
-                        item.GetComponent<SpriteRenderer>().sprite = coffee;
-                        break;
-                    case ItemType.Clipboard:
-                        item.GetComponent<SpriteRenderer>().sprite = clipboard;
-                        break;
-                    case ItemType.Stapler:
-                        item.GetComponent<SpriteRenderer>().sprite = stapler;
-                        break;
-                    case ItemType.PencilHolder:
-                        item.GetComponent<SpriteRenderer>().sprite = pencilHolder;
-                        break;
-                    case ItemType.PaperClip:
-                        item.GetComponent<SpriteRenderer>().sprite = paperclip;
-                        break;
-                    case ItemType.Scissor:
-                        item.GetComponent<SpriteRenderer>().sprite = scissor;
-                        break;
-                    case ItemType.Eraser:
-                        item.GetComponent<SpriteRenderer>().sprite = eraser;
-                        break;
+            if (!holdingItem)
+            {
+                if (Keyboard.current.spaceKey.wasPressedThisFrame)
+                {   
+                    holdingItem = true;
+                    switch (itemType.type)
+                    {
+                        case ItemType.Water:
+                            item.GetComponent<SpriteRenderer>().sprite = water;
+                            break;
+                        case ItemType.Paper:
+                            item.GetComponent<SpriteRenderer>().sprite = paper;
+                            break;
+                        case ItemType.Coffee:
+                            item.GetComponent<SpriteRenderer>().sprite = coffee;
+                            break;
+                        case ItemType.Clipboard:
+                            item.GetComponent<SpriteRenderer>().sprite = clipboard;
+                            break;
+                        case ItemType.Stapler:
+                            item.GetComponent<SpriteRenderer>().sprite = stapler;
+                            break;
+                        case ItemType.PencilHolder:
+                            item.GetComponent<SpriteRenderer>().sprite = pencilHolder;
+                            break;
+                        case ItemType.PaperClip:
+                            item.GetComponent<SpriteRenderer>().sprite = paperclip;
+                            break;
+                        case ItemType.Scissor:
+                            item.GetComponent<SpriteRenderer>().sprite = scissor;
+                            break;
+                        case ItemType.Eraser:
+                            item.GetComponent<SpriteRenderer>().sprite = eraser;
+                            break;
+                    }
+                    Debug.Log(itemType.type);
+                    this.item.SetActive(true);
                 }
-                Debug.Log(itemType.type);
-                this.item.SetActive(true);
-                
             }
         } 
         if (inDeliveryZone)
